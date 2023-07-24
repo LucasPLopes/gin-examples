db1a06aab8642ea4311a3de00cdf8db445f05811
lucas <l.lopes.fga@gmail.com>
2023-07-23 23:50:44 -0300

    basic example

---
+++ basic/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]string)
+
+type User struct {
+	User   string `json:"user"`
+	Info   string `json:"info"`
+	Status string `json:"status"`
+}
+
+func router() *gin.Engine {
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		info, ok := db[user]
+		if ok {
+			c.JSON(http.StatusOK,
+				User{
+					User: user,
+					Info: info,
+				})
+		} else {
+			c.JSON(http.StatusOK,
+				User{
+					User:   user,
+					Status: "no info",
+				})
+		}
+	})
+
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":   "bar",      // Zm9vOmJhcg==
+		"admin": "password", // YWRtaW46cGFzc3dvcmQ=
+	}))
+
+	authorized.POST("admin", func(c *gin.Context) {
+		// user := c.MustGet(gin.AuthUserKey).(string)
+
+		var json struct {
+			User string `json:"user" binding:"required"`
+			Info string `json:"info" binding:"required"`
+		}
+
+		if c.Bind(&json) == nil {
+			db[json.User] = json.Info
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		}
+	})
+
+	return r
+}
+
+func main() {
+	r := router()
+	r.Run(":8080")
+}
