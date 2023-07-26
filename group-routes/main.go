eb9645a59ed6730d5ac3e994bbc8c14e5d38885c
lucas <l.lopes.fga@gmail.com>
2023-07-26 02:58:23 -0300

    add group-routes example

---
+++ group-routes/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/LucasPLopes/gin-examples/group-routes/routes"
+
+func main() {
+	routes.Run()
+}
---
+++ group-routes/routes/users.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func addUserRoutes(rg *gin.RouterGroup) {
+	users := rg.Group("/users")
+
+	users.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "users")
+	})
+
+	users.GET("/comments", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "users comments")
+	})
+
+	users.GET("/pictures", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "users pictures")
+	})
+}
